9a5dcc02d8fcaf68f47f1ec6eaa4ca6e2896232e
ashutoshmjain <ashutoshmjain@users.noreply.github.com>
2026-07-09 00:26:31 +0000

    deploy: c4c99e00ad4b07d4b568163e5230b70d6b8a371d

--- api/doc/md_publish/sidebar-items.js
+++ api/doc/md_publish/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["ingest_image","ingest_text","ingest_video","list_parked","main","park_episode","run_doctor","unpark_episode","update_summary"],"mod":["sanitizer"],"struct":["BookConfig","EpisodeEntry","IngestConfig","PreprocessorConfig"]};
+window.SIDEBAR_ITEMS = {"fn":["ingest_image","ingest_text","ingest_video","inject_video_carousel","list_parked","main","park_episode","run_doctor","unpark_episode","update_summary"],"mod":["sanitizer"],"struct":["BookConfig","EpisodeEntry","IngestConfig","PreprocessorConfig"]};
--- api/doc/trait.impl/core/clone/trait.Clone.js
+++ api/doc/trait.impl/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["md_publish",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"md_publish/struct.EpisodeEntry.html\" title=\"struct md_publish::EpisodeEntry\">EpisodeEntry</a>",0]]]]);
+    const implementors = Object.fromEntries([["md_publish",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"md_publish/struct.EpisodeEntry.html\" title=\"struct md_publish::EpisodeEntry\">EpisodeEntry</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
